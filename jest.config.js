7c58f32f2bf3e588c0938c837ae706be2e71d099
Jeff <JeffreyRiggle@gmail.com>
2020-07-09 07:33:05 -0400

    start using coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   verbose: true,
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest"
+  },
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 80,
+      lines: 80,
+      statements: -20
+    }
   }
 }
