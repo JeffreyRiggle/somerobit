b8868d6394177ec7c543182c89efedeb19566c5a
Jeff <JeffreyRiggle@gmail.com>
2020-07-20 21:21:03 -0400

    add untested files to coverage

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80
+      branches: 75,
+      functions: 75,
+      lines: 75
     }
-  }
+  },
+  collectCoverageFrom: ['src/**/*.js']
 }
